2140dc0ed4e11513907a257a199fc791656a1fa4
gokcedemet98@gmail.com <gokcedemet98@gmail.com>
2022-08-24 14:44:41 +0300

    _layout eklendi

---
+++ Domain/Migrations/20220824110628_init.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domain.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ders",
+                columns: table => new
+                {
+                    DersId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DersAd = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ders", x => x.DersId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kullanici",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    RePassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true, defaultValue: "user"),
+                    LoginDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kullanici", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ogrenciler",
+                columns: table => new
+                {
+                    OgrenciId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TcNo = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    EPosta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gsm = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DogumTarih = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GirisTarih = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CikisTarih = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogrenciler", x => x.OgrenciId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ogretmenler",
+                columns: table => new
+                {
+                    OgretmenId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TcNo = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    EPosta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gsm = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DogumTarih = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GirisTarih = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CikisTarih = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogretmenler", x => x.OgretmenId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Siniflar",
+                columns: table => new
+                {
+                    SiniflarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SinifAdi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Kapasite = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Siniflar", x => x.SiniflarId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veli",
+                columns: table => new
+                {
+                    VeliId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TcNo = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    EPosta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gsm = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veli", x => x.VeliId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OgretmenDers",
+                columns: table => new
+                {
+                    OgretmenId = table.Column<int>(type: "int", nullable: false),
+                    DersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OgretmenDers", x => new { x.DersId, x.OgretmenId });
+                    table.ForeignKey(
+                        name: "FK_OgretmenDers_Ders_OgretmenId",
+                        column: x => x.OgretmenId,
+                        principalTable: "Ders",
+                        principalColumn: "DersId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OgretmenDers_Ogretmenler_DersId",
+                        column: x => x.DersId,
+                        principalTable: "Ogretmenler",
+                        principalColumn: "OgretmenId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OgrenciSiniflar",
+                columns: table => new
+                {
+                    OgrenciId = table.Column<int>(type: "int", nullable: false),
+                    SiniflarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OgrenciSiniflar", x => new { x.OgrenciId, x.SiniflarId });
+                    table.ForeignKey(
+                        name: "FK_OgrenciSiniflar_Ogrenciler_OgrenciId",
+                        column: x => x.OgrenciId,
+                        principalTable: "Ogrenciler",
+                        principalColumn: "OgrenciId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OgrenciSiniflar_Siniflar_SiniflarId",
+                        column: x => x.SiniflarId,
+                        principalTable: "Siniflar",
+                        principalColumn: "SiniflarId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OgrenciSiniflar_SiniflarId",
+                table: "OgrenciSiniflar",
+                column: "SiniflarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OgretmenDers_OgretmenId",
+                table: "OgretmenDers",
+                column: "OgretmenId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kullanici");
+
+            migrationBuilder.DropTable(
+                name: "OgrenciSiniflar");
+
+            migrationBuilder.DropTable(
+                name: "OgretmenDers");
+
+            migrationBuilder.DropTable(
+                name: "Veli");
+
+            migrationBuilder.DropTable(
+                name: "Ogrenciler");
+
+            migrationBuilder.DropTable(
+                name: "Siniflar");
+
+            migrationBuilder.DropTable(
+                name: "Ders");
+
+            migrationBuilder.DropTable(
+                name: "Ogretmenler");
+        }
+    }
+}
